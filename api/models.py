17c641c5d718152277fddb3102b12ed755f04da1
Speedy3004 <redddd15@gmail.com>
2019-11-16 13:49:28 +0300

    add models

--- api/models.py
+++ api/models.py
@@ -1,1 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Script(models.Model):
+    title = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Stage(models.Model):
+    name = models.CharField(max_length=255)
+    order = models.IntegerField()
+    script = models.ForeignKey(Script, on_delete=models.CASCADE)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    stage = models.ForeignKey(Stage, on_delete=models.CASCADE)
+
+
+class Parameter(models.Model):
+    name = models.CharField(max_length=255)
+    value = models.TextField()
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
